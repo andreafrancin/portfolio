4f99c8a38e7c379df0637a3d834e8b0cf81097ee
andreafrancin <andfrancin@gmail.com>
2025-03-16 22:39:56 +0100

    Added Head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { LanguageProvider } from "@/context/LanguageContext";
 import "./globals.css";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" title="Andrea Francin">
+      <Head>
+        <title>Andrea Francin</title>
+        <meta name="Andrea Francin" content="Graphic Designer & Illustrator" />
+      </Head>
       <body>
         <LanguageProvider>{children}</LanguageProvider>
       </body>
