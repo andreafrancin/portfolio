29bb90fb5e7b641b2d94f9f323f7102ad13ca482
andreafrancin <andfrancin@gmail.com>
2025-03-17 16:10:15 +0100

    Workaround over routing

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,58 +1,84 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./index.module.scss";
-import Header from "../header";
-import useIsMobile from "../../hooks/useIsMobile";
+import logoWhite from "../../../assets/images/logo/logo_white.png";
+import logoColor from "../../../assets/images/logo/logo_color.png";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import LanguageSelector from "../header/lang-selector";
-import dynamic from "next/dynamic";
+import useHover from "@/hooks/useHover";
 
-const BurgerMenu = dynamic(() => import("../header/burger-menu"), {
-  ssr: false,
-});
-
-export type Page = "about" | "home" | "contact" | "design" | "illustration";
-
-interface LayoutInterface {
-  headerConfig?: {
-    displayPrimaryLinks?: boolean;
-    currentPage?: Page;
-  };
-  children: React.ReactNode;
-}
-
-function Layout({ headerConfig, children }: LayoutInterface) {
-  const [isClient, setIsClient] = useState(false);
+const BurgerMenu = () => {
+  const router = useRouter();
   const pathname = usePathname();
-  const isMobile = useIsMobile();
-  const { displayPrimaryLinks = true, currentPage = "home" } =
-    headerConfig || {};
+
+  const { ref, isHovered } = useHover<HTMLImageElement>();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleNavigation = useCallback(
+    (route: string) => {
+      if (pathname !== route) {
+        setIsOpen(false);
+        router.push(route);
+      }
+    },
+    [pathname, router]
+  );
 
   useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return <div className={styles.loadingWindow}></div>;
-  }
+    setIsOpen(false);
+  }, [pathname]);
 
   return (
-    <div className={styles.container} key={pathname}>
-      {isMobile ? (
-        <BurgerMenu />
-      ) : (
-        <>
-          <Header
-            currentPage={currentPage}
-            displayPrimaryLinks={displayPrimaryLinks}
+    <div className={`${styles.container} ${isOpen ? styles.open : ""}`}>
+      {isOpen && <LanguageSelector onClick={() => setIsOpen(false)} />}
+      <div className={styles.logoContainer}>
+        <button onClick={() => handleNavigation("/")}>
+          <Image
+            ref={ref}
+            src={isHovered ? logoColor : logoWhite}
+            className={styles.logo}
+            alt="logo"
           />
-          <LanguageSelector />
-        </>
-      )}
-      {children}
+        </button>
+      </div>
+      <div className={styles.burgerIcon} onClick={toggleMenu}>
+        <span className={styles.burgerLine}></span>
+        <span className={styles.burgerLine}></span>
+        <span className={styles.burgerLine}></span>
+      </div>
+      <div className={`${styles.menuItems} ${isOpen ? styles.show : ""}`}>
+        <ul>
+          <li>
+            <button onClick={() => handleNavigation("/")}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => handleNavigation("/about")}>About</button>
+          </li>
+          <li>
+            <button onClick={() => handleNavigation("/design")}>
+              Design & Illustration
+            </button>
+          </li>
+          <li>
+            <button onClick={() => handleNavigation("/illustration")}>
+              Illustration Artist
+            </button>
+          </li>
+          <li>
+            <button onClick={() => handleNavigation("/contact")}>
+              Contact
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
-export default Layout;
+export default BurgerMenu;
