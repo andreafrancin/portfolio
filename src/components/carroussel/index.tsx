ed7b6114fdc19a9442971d9f9ee080307777f146
Canada <canada@Guillems-MacBook-Pro.local>
2025-03-25 22:34:00 +0100

    First carroussel version done

--- src/components/carroussel/index.tsx
+++ src/components/carroussel/index.tsx
@@ -2,10 +2,9 @@ import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import styles from "./index.module.scss";
 import ArrowIcon from "@/assets/icons/Arrow";
 
-const Carroussel = ({ data }: {data: any}) => {
+const Carroussel = ({ data }: { data: any }) => {
     const [visibleData, setVisibleData] = useState<any>(null);
-    const [translateX, setTranslateX] = useState(0);
-    const contentRef = useRef<any>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
     const [currentIndex, setCurrentIndex] = useState(0);
     const touchStartX = useRef(0);
     const touchEndX = useRef(0);
@@ -25,39 +24,35 @@ const Carroussel = ({ data }: {data: any}) => {
     useEffect(() => {
         if (data) {
             setVisibleData(data);
-            setCurrentIndex(Math.floor(data?.image_resources?.length / 2));
         }
     }, [data]);
 
-    const onArrowClick = useCallback((direction: string) => {
-        if (contentRef.current && visibleData?.image_resources?.length > 0) {
-            let newTranslateX = 0;
-            let childWidth = contentRef.current.children[currentIndex].getBoundingClientRect().width + 20;
+    const onArrowClick = useCallback(
+        (direction: string) => {
+            if (contentRef.current && visibleData?.image_resources?.length > 0) {
+                let childWidth = contentRef.current.children[0].getBoundingClientRect().width + 20;
 
-            if (direction === "right" && canMoveRight) {
-                newTranslateX = -childWidth;
-                setCurrentIndex((prevState) => prevState + 1);
-            } else if (direction === "left" && canMoveLeft) {
-                newTranslateX = childWidth;
-                setCurrentIndex((prevState) => prevState - 1);
+                setCurrentIndex((prevIndex) => {
+                    let newIndex = prevIndex;
+                    if (direction === "right" && canMoveRight) {
+                        newIndex = prevIndex + 1;
+                    } else if (direction === "left" && canMoveLeft) {
+                        newIndex = prevIndex - 1;
+                    }
+
+                    contentRef.current?.scrollTo({ left: newIndex * childWidth, behavior: "smooth" });
+                    return newIndex;
+                });
             }
+        },
+        [canMoveRight, canMoveLeft, visibleData]
+    );
 
-            setTranslateX((prevState) => prevState + newTranslateX);
-        }
-    }, [currentIndex, canMoveLeft, canMoveRight, visibleData]);
-
-    useEffect(() => {
-        if (contentRef.current) {
-            contentRef.current.style.transform = `translateX(${translateX}px)`;
-        }
-    }, [translateX]);
-
-    // Funciones para manejar el swipe en móviles
-    const handleTouchStart = (e: any) => {
+    const handleTouchStart = (e: React.TouchEvent) => {
         touchStartX.current = e.touches[0].clientX;
     };
 
-    const handleTouchMove = (e: any) => {
+    const handleTouchMove = (e: React.TouchEvent) => {
         touchEndX.current = e.touches[0].clientX;
     };
 
@@ -78,12 +73,17 @@ const Carroussel = ({ data }: {data: any}) => {
                 onTouchStart={handleTouchStart}
                 onTouchMove={handleTouchMove}
                 onTouchEnd={handleTouchEnd}
+                style={{ display: "flex", scrollBehavior: "smooth" }}
             >
                 {visibleData?.image_resources?.map((item: any, index: number) => (
                     <div 
                         key={index} 
                         className={styles.item} 
-                        style={{ opacity: index === currentIndex ? 1 : 0, transition: "opacity 0.2s ease-in-out" }}
+                        style={{ 
+                            minWidth: "100%", 
+                            transition: "opacity 0.2s ease-in-out", 
+                            opacity: index === currentIndex ? 1 : 0
+                        }}
                     >
                         <img src={item.image_url} alt={`Project Image ${index + 1}`} />
                     </div>
